40beae1f4efaf7675d0dfdc8b7f5baa84a286799
elert.2020 <elert.2020@corp.nstu.ru>
2024-02-18 15:47:49 +0700

    refactoring_v2

--- api/routes/protected/hsn/cabinet/__init__.py
+++ api/routes/protected/hsn/cabinet/__init__.py
@@ -1,8 +1,9 @@
 from .create import *
-from .get_by_id import *
+#from .get_by_id import *
 from .delete import *
 from .get_list import *
 from .update import *
+from .own import *
 from .router import cabinet_router
 
 __all__ = [
