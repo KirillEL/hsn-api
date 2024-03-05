c49db6a80ab818f02f52c0c3f94a1774f9f8ad44
elert.2020 <elert.2020@corp.nstu.ru>
2024-03-05 13:55:40 +0700

    update revision id

--- migrations/versions/20240226_0748_clinical_assesments.py
+++ migrations/versions/20240226_0748_clinical_assesments.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'da19609011a8'
-down_revision: Union[str, None] = '006f92681953'
+down_revision: Union[str, None] = '462667f47011'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
