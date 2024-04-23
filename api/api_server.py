3916206afd4b3b556d527cdf553b6208805ad2e7
elert.2020 <elert.2020@corp.nstu.ru>
2024-04-23 11:34:48 +0700

    refactoring cors

--- api/api_server.py
+++ api/api_server.py
@@ -36,8 +36,8 @@ def init_middlewares() -> List[Middleware]:
     middlewares: List[Middleware] = [
         Middleware(
             CORSMiddleware,
-            allow_origins=["http://localhost:5174", "http://hsn_admin:5174", "http://82.146.59.229:5174",
-                           "http://localhost:1111", "http://localhost:3000", "http://62.109.31.151:3000/"],
+            allow_origins=["http://localhost:5174", "http://hsn_admin:5174",
+                           "http://localhost:1111", "http://localhost:3000", "http://62.109.31.151:3000"],
             allow_credentials=True,
             allow_methods=["PUT", "POST", "GET", "DELETE", "OPTIONS", "PATCH"],
             allow_headers=["*"]
