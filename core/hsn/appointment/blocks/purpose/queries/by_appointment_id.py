2e9be13024ead9b4c9c0bc53a3c110ddd8232865
elert.2020 <elert.2020@corp.nstu.ru>
2024-05-15 17:13:22 +0700

    fix get_purpose by appointment_id

--- core/hsn/appointment/blocks/purpose/queries/by_appointment_id.py
+++ core/hsn/appointment/blocks/purpose/queries/by_appointment_id.py
@@ -7,6 +7,7 @@ from api.exceptions import NotFoundException, InternalServerException
 from api.exceptions.base import UnprocessableEntityException
 from core.hsn.appointment.blocks.purpose import AppointmentPurposeFlat
 from core.hsn.appointment.blocks.purpose.commands.create import check_appointment_exists
+from shared.db.models import MedicinesPrescriptionDBModel
 from shared.db.models.appointment.purpose import AppointmentPurposeDBModel
 from shared.db.db_session import db_session, SessionContext
 
@@ -17,7 +18,7 @@ async def hsn_get_purposes_by_appointment_id(appointment_id: int):
     await check_appointment_exists(appointment_id)
     query = (
         select(AppointmentPurposeDBModel)
-        .options(selectinload(AppointmentPurposeDBModel.medicine_prescription))
+        .options(selectinload(AppointmentPurposeDBModel.medicine_prescription).selectinload(MedicinesPrescriptionDBModel.medicine_group))
         .where(AppointmentPurposeDBModel.appointment_id == appointment_id)
         .where(AppointmentPurposeDBModel.is_deleted.is_(False))
     )
